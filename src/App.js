988d9c2f5abd94420da41d13295fb15deb803ff0
Michael McAlroy <michael.mcalroy@gmail.com>
2020-01-01 10:33:11 +0000

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
+
 import "./App.css";
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => {
+  return (<h1>HATS</h1>);
+};
+
 const App = () => (
   <div>
-    <HomePage />
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/shop/hats" component={HatsPage} />
+    </Switch>
   </div>
 );
 
